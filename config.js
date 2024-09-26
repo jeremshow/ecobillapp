636ffce76b6d83fb44d8a58f986676ec526025d2
Jérémy Langlois <jeremylanglois@MacBook-Pro-de-Jeremy.local>
2024-09-26 18:51:41 -0400

    Update database connection to use environment variables

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://ecobillpay_user:Ak2mHkddTDZfGpdrXhGGzz5Z3ahrP2bI@dpg-crqu765ds78s73cnuo9g-a:5432/ecobillpay', // Remplacez par les informations de votre base de données
-  ssl: {
-    rejectUnauthorized: false
-  }
+    host: process.env.DB_HOST || 'dpg-crqu765ds78s73cnuo9g-a',
+    user: process.env.DB_USER || 'ecobillpay_user',
+    password: process.env.DB_PASSWORD || 'Ak2mHkddTDZfGpdrXhGGzz5Z3ahrP2bI',
+    database: process.env.DB_NAME || 'ecobillpay',
+    port: process.env.DB_PORT || 5432, // généralement 5432 pour PostgreSQL
 });
 
 module.exports = pool;
