c1decb3b089a07da04d8642618ab1f7fd1b8584b
Jérémy Langlois <jeremylanglois@MacBook-Pro-de-Jeremy.local>
2024-09-26 18:48:35 -0400

    Update database configuration for Render

--- config.js
+++ config.js
@@ -1,28 +1,10 @@
-require('dotenv').config();
+const { Pool } = require('pg');
 
-module.exports = {
-    development: {
-        username: 'postgres',        // Remplacez par votre nom d'utilisateur
-        password: '250104Jl',        // Remplacez par votre mot de passe
-        database: 'ecobillpay',      // Nom de votre base de données
-        host: 'localhost',           // Ou l'adresse IP de votre serveur PostgreSQL
-        dialect: 'postgres',         // Dialecte de la base de données
-        port: 5451                    // Remplacez par le port si nécessaire (5432 est le port par défaut)
-    },
-    test: {
-        username: 'postgres',
-        password: '250104Jl',
-        database: 'ecobillpay_test',
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5451
-    },
-    production: {
-        username: 'postgres',
-        password: '250104Jl',
-        database: 'ecobillpay_production',
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5451
-    }
-};
+const pool = new Pool({
+  connectionString: 'postgres://ecobillpay_user:Ak2mHkddTDZfGpdrXhGGzz5Z3ahrP2bI@dpg-crqu765ds78s73cnuo9g-a:5432/ecobillpay', // Remplacez par les informations de votre base de données
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
