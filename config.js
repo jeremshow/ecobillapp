9e9e9ec0900951eb5e92c0f1e99c8980b1bf7a53
Jérémy Langlois <jeremylanglois@MacBook-Pro-de-Jeremy.local>
2024-09-26 19:05:12 -0400

    Update database configuration

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+// Remplacez ceci par l'URL de connexion fournie par Render
 const pool = new Pool({
-    host: process.env.DB_HOST || 'dpg-crqu765ds78s73cnuo9g-a',
-    user: process.env.DB_USER || 'ecobillpay_user',
-    password: process.env.DB_PASSWORD || 'Ak2mHkddTDZfGpdrXhGGzz5Z3ahrP2bI',
-    database: process.env.DB_NAME || 'ecobillpay',
-    port: process.env.DB_PORT || 5432, // généralement 5432 pour PostgreSQL
+  connectionString: 'postgresql://ecobillpay_user:Ak2mHkddTDZfGpdrXhGGzz5Z3ahrP2bI@dpg-crqu765ds78s73cnuo9g-a.oregon-postgres.render.com/ecobillpay',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
